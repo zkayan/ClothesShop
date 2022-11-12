f03dad443659037db4581a1662a7cc96c851bb5f
Kayan Chagas <67170002+zkayan@users.noreply.github.com>
2022-11-12 16:31:38 -0300

    Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _speed = 2f;
+    private Rigidbody2D _rigidbody;
+    private Vector3 _playerMovement;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _playerMovement = Vector3.zero;
+        _playerMovement.x = Input.GetAxisRaw("Horizontal");
+        _playerMovement.y = Input.GetAxisRaw("Vertical");
+        _playerMovement = Vector3.ClampMagnitude(_playerMovement, 1f);
+
+        UpdateAnimationAndMove();
+    }
+
+    private void UpdateAnimationAndMove()
+    {
+        if (_playerMovement != Vector3.zero)
+        {
+            MoveCharacter();
+            _animator.SetFloat("MoveX", _playerMovement.x);
+            _animator.SetFloat("MoveY", _playerMovement.y);
+            _animator.SetBool("Walking", true);
+        }
+        else
+        {
+            _animator.SetBool("Walking", false);
+        }
+    }
+
+    private void MoveCharacter()
+    {
+        _rigidbody.MovePosition(transform.position + _playerMovement * _speed * Time.deltaTime);
+    }
+}
